1a9093199a07dd3ea03c0d8501bade27c5cbb6cb
Mark Ramage <mramage@hotmail.co.uk>
2022-03-08 11:12:45 +0000

    Create card related migrations

---
+++ database/migrations/2022_03_08_105920_create_cards_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('rank_id');
+            $table->integer('suit_id');
+            $table->foreign('rank_id')->references('id')->on('ranks');
+            $table->foreign('suit_id')->references('id')->on('suits');
+            $table->unique(['rank_id', 'suit_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cards');
+    }
+}
